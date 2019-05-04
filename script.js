963214b23565ccb29235f9018a39d7e2a46ff6db
mhansi <srsmsone@gmail.com>
2019-05-05 01:07:34 +0530

    add score counting feature and some other features.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard =false;
 let lockBoard = false;
 let firstCard, secondCard;
+let score =0;
+let win=0;
 cards.forEach(card=>card.addEventListener('click',flipCard));
 
 function flipCard(){
@@ -25,12 +27,21 @@ function flipCard(){
    
    }
    function disableCards(){
+       score+=2;
+       win++
+       if(win==6){
+           $("#restart-div").slideDown();
+           document.getElementById("final_score").innerHTML="Score:"+score;
+       }
+       document.getElementById("score").innerHTML="Score:"+score;
     firstCard.removeEventListner('click',flipCard());
     secondCard.removeEventListner('click',flipCard());
 
     resetBoard();
    }
    function unFlipCards(){
+       score--;
+       document.getElementById("score").innerHTML="Score:"+score;
        lockBoard=true;
     setTimeout(()=>{
         firstCard.classList.remove('flip');
